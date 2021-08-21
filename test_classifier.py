743fd787fa56a663a5a57ff82657e4deb015ebe2
Vinh Nguyen <vinnee.nguyen@gmail.com>
2021-08-21 19:36:22 +0000

    update test_classifier.py

--- test_classifier.py
+++ test_classifier.py
@@ -31,12 +31,47 @@ image_urls = [
 def classifier():
     return BirdClassifier()
 
+@pytest.mark.parametrize('url', [
+    123, True, [], {}
+])
+def test_load_image_wrong_type(classifier, url):
+    with pytest.raises(Exception) as e_info:
+        image = classifier.load_image(url)
+
+@pytest.mark.parametrize('url', [
+    "123", "abc", ""
+])
+def test_load_image_invalid_url(classifier, url):
+    with pytest.raises(Exception) as e_info:
+        image = classifier.load_image(url)
+
 @pytest.mark.parametrize('url', image_urls)
 def test_load_image(classifier, url):
     image = classifier.load_image(url)
     assert image.shape == (224, 224, 3)
     assert np.all(image <= 1)  # normalized
     
+@pytest.mark.parametrize('url', image_urls)
+def test_load_image_file_object(classifier, url):
+    fobj = urllib.request.urlopen(url)
+    image = classifier.load_image(fobj)
+    assert image.shape == (224, 224, 3)
+    assert np.all(image <= 1)  # normalized
+
+@pytest.mark.parametrize('image_url', [
+    123, True, [], {}
+])
+def test_identify_wrong_type(classifier, image_url):
+    with pytest.raises(Exception) as e_info:
+        top_names, top_scores = classifier.identify(image_url)
+
+@pytest.mark.parametrize('image_url', [
+    "123", "abc", ""
+])
+def test_identify_invalid_image_url(classifier, image_url):
+    with pytest.raises(Exception) as e_info:
+        top_names, top_scores = classifier.identify(image_url)
+
 @pytest.mark.parametrize('image_url,exp_top_3_names', [
     ('https://upload.wikimedia.org/wikipedia/commons/c/c8/Phalacrocorax_varius_-Waikawa%2C_Marlborough%2C_New_Zealand-8.jpg',
      ["Phalacrocorax varius varius", "Phalacrocorax varius", "Microcarbo melanoleucos"]),
@@ -51,6 +86,25 @@ def test_load_image(classifier, url):
 ])
 def test_identify(classifier, image_url, exp_top_3_names):
     top_names, top_scores = classifier.identify(image_url)
+    npt.assert_equal(top_names[:3], exp_top_3_names)
+    npt.assert_array_less(top_scores[1:]/top_scores[0], 0.5)  # top probability more than twice others
+    npt.assert_almost_equal(sum(top_scores), 1, decimal=5)  # total probability 1
+
+@pytest.mark.parametrize('image_url,exp_top_3_names', [
+    ('https://upload.wikimedia.org/wikipedia/commons/c/c8/Phalacrocorax_varius_-Waikawa%2C_Marlborough%2C_New_Zealand-8.jpg',
+     ["Phalacrocorax varius varius", "Phalacrocorax varius", "Microcarbo melanoleucos"]),
+    ('https://quiz.natureid.no/bird/db_media/eBook/679edc606d9a363f775dabf0497d31de8c3d7060.jpg',
+     ["Galerida cristata", "Alauda arvensis", "Eremophila alpestris"]),
+    ('https://upload.wikimedia.org/wikipedia/commons/8/81/Eumomota_superciliosa.jpg',
+     ["Eumomota superciliosa", "Momotus coeruliceps", "Momotus lessonii"]),
+    ('https://i.pinimg.com/originals/f3/fb/92/f3fb92afce5ddff09a7370d90d021225.jpg',
+     ["Aulacorhynchus prasinus", "Cyanocorax yncas", "Chlorophanes spiza"]),
+    ('https://cdn.britannica.com/77/189277-004-0A3BC3D4.jpg',
+     ["Erithacus rubecula", "Ixoreus naevius", "Setophaga tigrina"])
+])
+def test_identify_file_object(classifier, image_url, exp_top_3_names):
+    fobj = urllib.request.urlopen(image_url)
+    top_names, top_scores = classifier.identify(fobj)
     npt.assert_equal(top_names[:3], exp_top_3_names)
     npt.assert_array_less(top_scores[1:]/top_scores[0], 0.5)  # top probability more than twice others
     npt.assert_almost_equal(sum(top_scores), 1, decimal=5)  # total probability 1
